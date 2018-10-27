24734ff83820c4029660bbab7a03cd3eafa52f34
Gerrad Gentzel <ggentzel@focustapss.com>
2018-10-27 15:56:05 -0400

    Gargoyle

---
+++ Assets/_Scene/game/enemies/gargoyle/Fireball.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour {
+	public float Speed = 10;
+	public Vector3 Direction = new Vector3(-1, 0, 0);
+	Rigidbody2D rbody;
+	// Use this for initialization
+	void Start () {
+		this.rbody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rbody.MovePosition(transform.position + this.Direction * this.Speed * Time.deltaTime);
+	}
+
+	void OnCollisionEnter2D(Collision2D col){
+		Destroy(gameObject);
+	}
+}
---
+++ Assets/_Scene/game/enemies/gargoyle/Gargoyle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gargoyle : MonoBehaviour {
+	public GameObject Fireball;
+	public GameObject FireballEmitter;
+	public float FireRate = 10;
+	float fireCooldown = 0;
+	// Update is called once per frame
+	void Update () {
+		this.fireCooldown += Time.deltaTime;
+		if(this.fireCooldown >= this.FireRate){
+			//spawn fireball
+			GameObject.Instantiate(this.Fireball, this.FireballEmitter.transform.position, Quaternion.identity);
+			this.fireCooldown = 0;
+		}
+	}
+}
