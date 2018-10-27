3d4a8e545b1e0bdcb0baf84d3c8f816e99ca8310
Gerrad Gentzel <ggentzel@focustapss.com>
2018-10-27 17:28:41 -0400

    Patrolling Enemy

--- Assets/_Scene/game/enemies/PomWolf/PatrollingEnemy.cs
+++ Assets/_Scene/game/enemies/PomWolf/PatrollingEnemy.cs
@@ -1,23 +1,49 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PatrollingEnemy : MonoBehaviour {
-	public Transform CurrentTarget;
+	Transform currentTarget;
+	public GameObject Targets;
+	public int currentTargetInt = 0;
+	public Transform[] targetsList;
 	public float Speed = 5;
 	Animator anim;
 	Rigidbody2D rbody;
 	// Use this for initialization
 	void Start () {
 		this.anim = GetComponent<Animator>();
+		this.rbody = GetComponent<Rigidbody2D>();
+		var temp = this.Targets.GetComponentsInChildren<Transform>();
+		List<Transform> t = new List<Transform>();
+		for(int i = 0; i < temp.Length; i++){
+			if(i > 0){
+				t.Add(temp[i]);
+			}
+		}
+		t.Sort((Transform a, Transform b) => {
+			return a.name.CompareTo(b.name);
+		});
+		this.targetsList = t.ToArray();guiText
+		this.currentTarget = this.targetsList[0];
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 facing = CurrentTarget.position - transform.position;
+		Vector3 facing = currentTarget.position - transform.position;
 		facing.Normalize();
 		anim.SetFloat("horizontal", facing.x);
 		anim.SetFloat("vertical", facing.y);
-		rbody.MovePosition(transform.position + facing * this.Speed * Time.deltaTime);
+		if(Mathf.Abs((transform.position - currentTarget.position).magnitude) > .1){
+			rbody.MovePosition(transform.position + facing * this.Speed * Time.deltaTime);
+		}
+		else{
+			this.currentTargetInt++;
+			if(this.currentTargetInt >= this.targetsList.Length)
+				this.currentTargetInt = 0;
+			this.currentTarget = this.targetsList[this.currentTargetInt];
+		}
+		
 	}
 }
