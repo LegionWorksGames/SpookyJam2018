68cdc63f30cf632cf0f39d5745ad1609c4ec7f41
Brett Maxwell <bmaxwell112@gmail.com>
2018-10-27 18:52:40 -0400

    Added switch and removable wall

---
+++ Assets/_Scene/game/Switch/Switch.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour {
+
+    Animator anim;
+    [SerializeField] GameObject wall;
+    public bool switchOn;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();	
+	}
+
+    private void Update()
+    {
+        anim.SetBool("on", switchOn);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            switchOn = !switchOn;
+            if(switchOn)
+            {
+                wall.SetActive(false);
+            }
+            else
+            {
+                wall.SetActive(true);
+            }
+        }
+    }
+}
